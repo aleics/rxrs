eb28894e65f2208c1931e1f688aa74ab9e23731f
aleics <aleics.casanovas@gmail.com>
2019-06-27 18:22:04 +0200

    Define closed subscription flag

--- src/subscription.rs
+++ src/subscription.rs
@@ -1,22 +1,45 @@
 
 
 pub struct Subscription {
+    pub closed: bool
 }
 
 impl Subscription {
     pub fn new() -> Subscription {
-        Subscription { }
+        Subscription { closed: true }
     }
 
     pub fn subscribe(&mut self) {
-        unimplemented!();
+        self.closed = false;
     }
 
     pub fn unsubscribe(&mut self) {
-        unimplemented!();
+        self.closed = true;
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::Subscription;
+
+    #[test]
+    fn new() {
+        let subscription = Subscription::new();
+        assert_eq!(subscription.closed, true);
+    }
+
+    #[test]
+    fn subscribe() {
+        let mut subscription = Subscription::new();
+        subscription.subscribe();
+        assert_eq!(subscription.closed, false);
+    }
+
+    #[test]
+    fn unsubscribe() {
+        let mut subscription = Subscription::new();
+        subscription.subscribe();
+        subscription.unsubscribe();
+        assert_eq!(subscription.closed, true);
+    }
 }
