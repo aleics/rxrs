b8059f1c90f20364eb2556d2a973b7fb4f13b3aa
aleics <aleics.casanovas@gmail.com>
2019-07-14 17:49:14 +0200

    Include filter example

--- examples/observable-ops.rs
+++ examples/observable-ops.rs
@@ -1,8 +1,13 @@
-use rxrs::operators::{of, map};
+use rxrs::operators::{of, map, filter};
 
 fn main() {
-    let obs = of(&[1, 2, 3]);
-    obs.pipe(
-        map(|item: &i32| item * 2)
-    ).subscribe_next(|item| println!("{}", item));
+    let obs_map = of(&[1, 2, 3]);
+    obs_map.pipe(
+        map(|item| item * 2)
+    ).subscribe_next(|item| println!("map: {}", item));
+
+    let obs_filter = of(&[1, 2, 3]);
+    obs_filter.pipe(
+        filter(|item: &i32| item.clone() > 1)
+    ).subscribe_next(|item| println!("filter: {}", item));
 }
