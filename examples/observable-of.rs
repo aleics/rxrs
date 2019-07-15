a76f73e422be87f6cb185c4fc87cc8c7982d9863
aleics <aleics.casanovas@gmail.com>
2019-07-15 19:56:23 +0200

    Include of example using data structures

--- examples/observable-of.rs
+++ examples/observable-of.rs
@@ -1,11 +1,26 @@
-use rxrs::operators::of;
+use rxrs::operators::{of, map};
+
+#[derive(Debug)]
+struct DataItem {
+    value: i32
+}
 
 fn main() {
-    let obs = of(&[1, 2, 3]);
+    let obs_static = of(&[1, 2, 3]);
 
-    obs.subscribe_next(|value| println!("first: {}", value));
+    obs_static.subscribe_next(|value| println!("first: {}", value));
     println!("after first");
 
-    obs.subscribe_next(|value| println!("second: {}", value));
+    obs_static.subscribe_next(|value| println!("second: {}", value));
     println!("after second");
+
+    let data = [
+        DataItem { value: 1 },
+        DataItem { value: 2 },
+        DataItem { value: 3 }
+    ];
+    let obs_data = of(&data);
+    obs_data.pipe(
+        map(|item: &DataItem| DataItem { value: item.value + 1})
+    ).subscribe_next(|item| println!("{:?}", item));
 }
